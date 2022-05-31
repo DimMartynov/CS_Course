dc9e4d29c9411617ae16db0acb3aa03a84f1d1e0
DimMartynov <103519243+DimMartynov@users.noreply.github.com>
2022-05-31 17:57:16 +0300

    Add files via upload

---
+++ CS_Course/Lab8/task1_2/BankTransaction.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+public class BankTransaction
+{
+    private readonly decimal amount;
+    private readonly DateTime when;
+    public decimal Amount( )
+    {
+        return amount;
+    }
+
+    public DateTime When( )
+    {
+        return when;
+    }
+    public BankTransaction(decimal tranAmount)
+    {
+        amount = tranAmount;
+        when = DateTime.Now;
+    }
+    
+}
